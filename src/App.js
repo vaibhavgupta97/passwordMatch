009f3fbad672e16d3c9bcb2bdf408a784155a838
vaibhav <vaibhav16feb1997@gmail.com>
2021-04-08 15:09:22 +0530

    formatting documents

--- src/App.js
+++ src/App.js
@@ -1,31 +1,53 @@
-import {useState} from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [password,setPassword]=useState("")
-  const [confirmPassword,setConfirnPassword]=useState("")
- const handleChange=(e)=>{
-    setPassword(e.target.value)
-   // console.log(password.toString());
-  }
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirnPassword] = useState("");
+  const handleChange = (e) => {
+    setPassword(e.target.value);
+    // console.log(password.toString());
+  };
 
-const handlePass=(e)=>{
-    setConfirnPassword(e.target.value)
+  const handlePass = (e) => {
+    setConfirnPassword(e.target.value);
     //console.log(confirmPassword)
-  }
+  };
   return (
     <div className="app">
-       
-    {(password.toString()===confirmPassword.toString() && password.length!==0)?
-    (<form className="passwordForm">
-    <input type="password" placeholder="Please enter the password" onChange={handleChange}  name="password1" value={password}/>
-    <input type="password" placeholder="Confirm Password" onChange={handlePass} name="password2" value={confirmPassword}/>
-    <button>Submit</button></form>):<form className="passwordForm">
-    <input type="password" placeholder="Please enter the password" onChange={handleChange}  name="password1" value={password}/>
-    <input type="password" placeholder="Confirm Password" onChange={handlePass} name="password2" value={confirmPassword}/>
-    </form>
-    }
-   
+      {password.toString() === confirmPassword.toString() &&
+      password.length !== 0 ? (
+        <form className="passwordForm">
+          <input
+            type="password"
+            placeholder="Please enter the password"
+            onChange={handleChange}
+            value={password}
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            onChange={handlePass}
+            value={confirmPassword}
+          />
+          <button>Submit</button>
+        </form>
+      ) : (
+        <form className="passwordForm">
+          <input
+            type="password"
+            placeholder="Please enter the password"
+            onChange={handleChange}
+            value={password}
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            onChange={handlePass}
+            value={confirmPassword}
+          />
+        </form>
+      )}
     </div>
   );
 }
