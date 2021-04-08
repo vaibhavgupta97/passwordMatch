e059c6ffd9a13ef69e0334cb7b82b581ed2d4a19
vaibhav <vaibhav16feb1997@gmail.com>
2021-04-08 15:05:15 +0530

    added styles

--- src/App.js
+++ src/App.js
@@ -4,30 +4,28 @@ import './App.css';
 function App() {
   const [password,setPassword]=useState("")
   const [confirmPassword,setConfirnPassword]=useState("")
- 
-  const handleChange=(e)=>{
+ const handleChange=(e)=>{
     setPassword(e.target.value)
    // console.log(password.toString());
   }
 
-  
-  const handlePass=(e)=>{
+const handlePass=(e)=>{
     setConfirnPassword(e.target.value)
     //console.log(confirmPassword)
   }
   return (
     <div className="app">
+       
     {(password.toString()===confirmPassword.toString() && password.length!==0)?
-    (<div>
+    (<form className="passwordForm">
     <input type="password" placeholder="Please enter the password" onChange={handleChange}  name="password1" value={password}/>
     <input type="password" placeholder="Confirm Password" onChange={handlePass} name="password2" value={confirmPassword}/>
-    <button>Submit</button></div>):<div>
+    <button>Submit</button></form>):<form className="passwordForm">
     <input type="password" placeholder="Please enter the password" onChange={handleChange}  name="password1" value={password}/>
     <input type="password" placeholder="Confirm Password" onChange={handlePass} name="password2" value={confirmPassword}/>
-    </div>
+    </form>
     }
    
-  
     </div>
   );
 }
